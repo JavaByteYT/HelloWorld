045f061b3703808229d5c07a3fb4b32bdf110169
Matthew Matter <Gh0stByte01@gmail.com>
2017-09-30 22:53:02 -0400

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * @author Amit Merchant
+ * September 5, 2017
+ * This program prints out a simple message "Hello World!"
+ */
+
+public class HelloWorld
+{
+    /**
+     * Print out the value of message on run. No input.
+     * @param args Main program arguments
+     */
+    public static void main(String[] args) {
+        // Define a message string that will contain the message to be printed
+        String message = "Hello World!";
+        // Print the message to the default system output
+        System.out.println(message);
+    }
+}
